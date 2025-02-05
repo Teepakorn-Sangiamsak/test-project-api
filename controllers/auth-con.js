4b478415800c935458d77f72d9ff1e8217025b4d
Iam-Jacobbbbbbb <teepakorn.snis@gmail.com>
2025-02-05 11:09:18 +0700

    msg4

--- controllers/auth-con.js
+++ controllers/auth-con.js
@@ -1,5 +1,12 @@
 exports.register = (req,res,next)=>{
     try{
+        // Step 1 req.body
+        console.log(req.body)
+        // Step 2 validate
+        // Step 3 Check already
+        // Step 4 Encrypt bcrypt
+        // Step 5 Insert to DB
+        // Step 6 Response
         res.json({message:"hello register"})
     }catch(error){
         next(error)
